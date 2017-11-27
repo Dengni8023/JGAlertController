f9875543693cc348db7441250630e7a24722d899
dengni8023 <945835664@qq.com>
2017-11-27 20:24:04 +0800

    头文件引用方式判断处理, JGSourceBase更新

--- JGAlertController.podspec
+++ JGAlertController.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
 s.name          = "JGAlertController"
-s.version       = "0.0.1"
+s.version       = "0.0.2"
 
 s.summary       = "iOS 弹出系统Alert通用方法定义"
 s.description   = <<-DESC
--- JGAlertController/JGAlertController.h
+++ JGAlertController/JGAlertController.h
@@ -16,5 +16,10 @@ FOUNDATION_EXPORT const unsigned char JGAlertControllerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JGAlertController/PublicHeader.h>
 
+#if __has_include(<JGAlertController/JGAlertControllerImpl.h>)
+#import <JGAlertController/JGAlertControllerImpl.h>
+#import <JGAlertController/UIViewController+JGAlertController.h>
+#else
 #import "JGAlertControllerImpl.h"
 #import "UIViewController+JGAlertController.h"
+#endif
